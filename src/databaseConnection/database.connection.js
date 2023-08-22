238607f7ef3b56d6df760be6c082cb01909e55a2
m4arthu <luisarthir259@gmail.com>
2023-08-22 18:31:11 -0300

    bug fix: ssl codition passed

--- src/databaseConnection/database.connection.js
+++ src/databaseConnection/database.connection.js
@@ -6,5 +6,6 @@ dotenv.config()
 const databaseConnection = {
     connectionString: process.env.DATABASE_CONNECTION_URL
 }
+process.env.NODE_ENV ==="producion"? databaseConnection.ssl = true : databaseConnection.ssl = false
 
 export  const db  = new Pool(databaseConnection)
